be8108c99c17cde933717ddc60a1fbf5227b5f08
theWellHopeErr <ssuryarajan@gmail.com>
2020-03-21 00:34:33 +0530

    Delete Unwanted Files

--- t2.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://www.akc.org/dog-breeds/airedale-terrier/"
-resp = requests.get(url)
-soup = BeautifulSoup(resp.text, 'html.parser')
-span = soup.findAll("span", {"class": "attribute-list__description"})
-print(span[5].text)
